83fd5187dc0008ac77c8f685272f191b89edfd6f
adnanhussain8082 <120333722+adnanhussain8082@users.noreply.github.com>
2025-01-05 23:05:16 +0530

    Time: 4 ms (63.88%) | Memory: 71.5 MB (66.39%) - LeetSync

--- 878-shifting-letters/shifting-letters.cpp
+++ 878-shifting-letters/shifting-letters.cpp
@@ -1,13 +1,25 @@
 class Solution {
 public:
     string shiftingLetters(string s, vector<int>& shifts) {
-        int n = s.size();
+        // int n = s.size();
 
-        long long shift=accumulate(shifts.begin(),shifts.end(),0LL);
+        // long long shift=accumulate(shifts.begin(),shifts.end(),0LL);
 
-        for(int i=0;i<n;i++){
-            s[i] = 'a' + (s[i]-'a' + shift%26)%26;
-            shift -= shifts[i];
+        // for(int i=0;i<n;i++){
+        //     s[i] = 'a' + (s[i]-'a' + shift%26)%26;
+        //     shift -= shifts[i];
+        // }
+        // return s;
+
+
+        // 2nd approach does not use long long but works from n-1 to 0(reverse)
+
+        int n=s.size();
+        int shift=0;
+
+        for(int i=n-1;i>=0;i--){
+            shift = (shift + shifts[i])%26;
+            s[i] = 'a' + (s[i]-'a' + shift)%26;
         }
         return s;
     }
