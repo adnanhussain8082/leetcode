cfbbd86a00e270f82b1fe7c396d8a87bc0a59e05
adnanhussain8082 <120333722+adnanhussain8082@users.noreply.github.com>
2025-01-18 19:09:04 +0530

    Time: 12 ms (83.40%) | Memory: 20.1 MB (58.79%) - LeetSync

---
+++ 1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
@@ -0,0 +1,43 @@
+int dir[4][2] = {{0, 1}, {0, -1}, {1, 0}, { -1, 0}};
+class Solution {
+private:
+    void dfs(vector<vector<int>>& grid , int r , int c , vector<vector<int>>& dp , int cost , queue<pair<int,int>>& q){
+        int m=grid.size() , n=grid[0].size();
+
+        //base case
+        if(r<0 || r>=m || c<0 || c>=n || dp[r][c]!=INT_MAX) return;
+
+        dp[r][c]=cost;
+        q.push({r,c});
+        int nextDir=grid[r][c]-1;
+
+        dfs(grid , r+dir[nextDir][0] , c+dir[nextDir][1] , dp , cost , q);
+    }
+public:
+    int minCost(vector<vector<int>>& grid) {
+        int m=grid.size() , n=grid[0].size();
+        int cost=0;
+        vector<vector<int>> dp(m , vector<int>(n,INT_MAX));
+        queue<pair<int,int>> q;
+
+        dfs(grid , 0 , 0 , dp , 0 , q);
+
+        while(!q.empty()){
+            cost++;
+
+            int size=q.size();
+
+            for(int i=0;i<size;i++){
+                pair<int,int> p = q.front();
+                q.pop();
+                int r=p.first , c=p.second;
+
+                for(int j=0;j<4;j++){
+                    dfs(grid , r+dir[j][0] , c+dir[j][1] , dp , cost , q);
+                }
+            }
+        }
+
+        return dp[m-1][n-1];
+    }
+};
