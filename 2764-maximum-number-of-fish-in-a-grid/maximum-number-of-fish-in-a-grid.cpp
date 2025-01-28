b616437e1319831000728c02c08996169dbf5748
Adnan <120333722+adnanhussain8082@users.noreply.github.com>
2025-01-28 21:00:40 +0530

    Time: 14 ms (42.21%) | Memory: 96.6 MB (48.16%) - LeetSync

--- 2764-maximum-number-of-fish-in-a-grid/maximum-number-of-fish-in-a-grid.cpp
+++ 2764-maximum-number-of-fish-in-a-grid/maximum-number-of-fish-in-a-grid.cpp
@@ -10,7 +10,7 @@ private:
             int x = i + dir[k][0];
             int y = j + dir[k][1];
 
-            if(x>=0 && x<m && y>=0 && y<n && vis[x][y]==-1){
+            if(x>=0 && x<m && y>=0 && y<n && vis[x][y]==-1 && grid[x][y]>0){
                 dfs(x , y , m , n , curr , grid , vis);
             }
         }
@@ -22,17 +22,17 @@ public:
         vector<vector<int>> vis(m , vector<int>(n,-1));
         int ans=0;
 
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(grid[i][j]==0){
-                    vis[i][j]=1;  //mark the land cells as already visited
-                }
-            }
-        }
+        // for(int i=0;i<m;i++){
+        //     for(int j=0;j<n;j++){
+        //         if(grid[i][j]==0){
+        //             vis[i][j]=1;  //mark the land cells as already visited
+        //         }
+        //     }
+        // }
 
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(vis[i][j]==-1){    //not a land cell and is unvisited
+                if(vis[i][j]==-1 && grid[i][j]>0){    //not a land cell and is unvisited
                     int curr=0;
                     dfs(i , j , m , n , curr , grid , vis);
                     ans = max(ans , curr);
