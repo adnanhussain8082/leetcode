c94aa16efaac12d46f5c0173f12207a93845b4bc
Adnan <120333722+adnanhussain8082@users.noreply.github.com>
2025-02-21 12:04:56 +0530

    Time: 5 ms (86.92%) | Memory: 27.3 MB (80.56%) - LeetSync

--- 1387-find-elements-in-a-contaminated-binary-tree/find-elements-in-a-contaminated-binary-tree.cpp
+++ 1387-find-elements-in-a-contaminated-binary-tree/find-elements-in-a-contaminated-binary-tree.cpp
@@ -10,23 +10,61 @@
  * };
  */
 
+                         //dfs method
+
+// class FindElements {
+// private:
+
+//     unordered_set<int> seen;
+
+//     void dfs(TreeNode* root , int value){
+//         if(root==NULL) return;
+
+//         root->val=value;
+//         seen.insert(value);
+
+//         dfs(root->left , 2*root->val + 1);
+//         dfs(root->right , 2*root->val + 2); 
+//     }
+// public:
+//     FindElements(TreeNode* root) {
+//         dfs(root , 0);
+//     }
+    
+//     bool find(int target) {
+//         return seen.find(target) != seen.end();
+//     }
+// };
+
+
+                      
+                      //bfs method
+
 class FindElements {
 private:
-
     unordered_set<int> seen;
-
-    void dfs(TreeNode* root , int value){
-        if(root==NULL) return;
-
-        root->val=value;
-        seen.insert(value);
-
-        dfs(root->left , 2*root->val + 1);
-        dfs(root->right , 2*root->val + 2); 
-    }
 public:
     FindElements(TreeNode* root) {
-        dfs(root , 0);
+        queue<TreeNode*> q;
+        q.push(root);
+
+        root->val=0;
+
+        while(!q.empty()){
+            auto node = q.front();
+            q.pop();
+            seen.insert(node->val);
+
+            if(node->left){
+                node->left->val = 2*node->val + 1;
+                q.push(node->left);
+            }
+
+            if(node->right){
+                node->right->val = 2*node->val + 2;
+                q.push(node->right);
+            }
+        }
     }
     
     bool find(int target) {
