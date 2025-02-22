964bb551c2854a1f9f702fb2aa9271a5f5c3f3dc
Adnan <120333722+adnanhussain8082@users.noreply.github.com>
2025-02-22 12:03:57 +0530

    Time: 4 ms (50.33%) | Memory: 20.7 MB (93.87%) - LeetSync

---
+++ 1093-recover-a-tree-from-preorder-traversal/recover-a-tree-from-preorder-traversal.cpp
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* recoverFromPreorder(string traversal) {
+        int n=traversal.size();
+        vector<TreeNode*> stack;
+
+        for(int i=0 , level , val;i<n;){
+
+            for(level=0;traversal[i]=='-';i++){
+                level++;
+            }
+
+            for(val=0;traversal[i]!='-' && i<n;i++){
+                val = val*10 + (traversal[i]-'0');
+            }
+            TreeNode* node = new TreeNode(val);
+
+            while(stack.size() > level) stack.pop_back();
+
+            if(!stack.empty()){
+                if(!stack.back()->left) stack.back()->left = node;
+                else stack.back()->right = node;
+            }
+
+            stack.push_back(node);
+        }
+        return stack.empty() ? NULL : stack[0];
+    }
+};
