828510ed2f37e2d4645904635897675c5ed9fa35
Adnan <120333722+adnanhussain8082@users.noreply.github.com>
2025-03-03 23:44:27 +0530

    Time: 10 ms (35.38%) | Memory: 127.7 MB (87.50%) - LeetSync

--- 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.cpp
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.cpp
@@ -1,25 +1,55 @@
+// class Solution {
+// private:
+//     vector<int> usingExtraSpace(vector<int>& nums, int pivot, )
+// public:
+//     vector<int> pivotArray(vector<int>& nums, int pivot) {
+//         int n=nums.size();
+
+//         vector<int> ans;
+
+//         int cnt = count(nums.begin(),nums.end(),pivot);
+
+//         for(auto& it : nums){
+//             if(it < pivot) ans.push_back(it);
+//         }
+
+//         while(cnt){
+//             ans.push_back(pivot);
+//             cnt--;
+//         }
+
+//         for(auto& it : nums){
+//             if(it > pivot) ans.push_back(it);
+//         }
+
+//         return ans;
+//     }
+// };
+
+
 class Solution {
 public:
     vector<int> pivotArray(vector<int>& nums, int pivot) {
-        int n=nums.size();
-
-        vector<int> ans;
-
-        int cnt = count(nums.begin(),nums.end(),pivot);
-
-        for(auto& it : nums){
-            if(it < pivot) ans.push_back(it);
+        vector<int> result(nums.size(), 0);
+        int left = 0, right = nums.size() - 1;
+        
+        for (int i = 0, j = nums.size() - 1; i < nums.size(); ++i, --j) {
+            if (nums[i] < pivot) {
+                result[left] = nums[i];
+                left++;
+            }
+            
+            if (nums[j] > pivot) {
+                result[right] = nums[j];
+                right--;
+            }
         }
-
-        while(cnt){
-            ans.push_back(pivot);
-            cnt--;
+        
+        while (left <= right) {
+            result[left] = pivot;
+            left++;
         }
-
-        for(auto& it : nums){
-            if(it > pivot) ans.push_back(it);
-        }
-
-        return ans;
+        
+        return result;
     }
 };
