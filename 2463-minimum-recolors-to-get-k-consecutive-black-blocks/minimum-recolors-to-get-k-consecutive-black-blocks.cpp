efa5c29590f3b8fe11a8d836476f41e1f703fcdd
Adnan <120333722+adnanhussain8082@users.noreply.github.com>
2025-03-08 15:43:35 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (31.01%) - LeetSync

--- 2463-minimum-recolors-to-get-k-consecutive-black-blocks/minimum-recolors-to-get-k-consecutive-black-blocks.cpp
+++ 2463-minimum-recolors-to-get-k-consecutive-black-blocks/minimum-recolors-to-get-k-consecutive-black-blocks.cpp
@@ -2,30 +2,48 @@ class Solution {
 public:
     int minimumRecolors(string blocks, int k) {
         int n=blocks.size();
-        vector<int> map(n,0);
-        int cnt=0;
+
+                           //O(n) space nahhhhhh
+        // vector<int> map(n,0);
+        // int cnt=0;
+
+        // for(int i=0;i<n;i++){
+        //     if(blocks[i]=='W') cnt++;
+        //     map[i]=cnt;
+        // }
+
+        // if(cnt==0) return 0;
+
+        // int ans=INT_MAX;
+        // int i=0 , j=k-1;
+        // int temp;
+
+        // while(j<n){
+        //     if(i!=0){
+        //        temp = map[j]-map[i-1];
+        //     }
+        //     else{
+        //         temp = map[j];
+        //     }
+        //     ans = min(ans , temp);
+        //     i++ , j++;
+        // }
+        // return ans;
+
+                          //O(1) space
+
+        int whites=0;
+        int res=INT_MAX;
 
         for(int i=0;i<n;i++){
-            if(blocks[i]=='W') cnt++;
-            map[i]=cnt;
+            if(blocks[i]=='W') whites++;
+
+            // start subtracting from K to transition to next window
+            if(i >= k && blocks[i-k]=='W') whites--;
+
+            // start checking from K-1 as window is already formed 
+            if(i >= (k-1) ) res=min(res,whites);
         }
-
-        if(cnt==0) return 0;
-
-        int ans=INT_MAX;
-        int i=0 , j=k-1;
-        int temp;
-
-        while(j<n){
-            if(i!=0){
-               temp = map[j]-map[i-1];
-            }
-            else{
-                temp = map[j];
-            }
-            ans = min(ans , temp);
-            i++ , j++;
-        }
-        return ans;
+        return res;
     }
 };
