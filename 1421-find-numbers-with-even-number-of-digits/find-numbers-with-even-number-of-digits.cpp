b8263ff93697d9407515765fe2fb1b5b074fc375
Adnan <120333722+adnanhussain8082@users.noreply.github.com>
2025-04-30 18:06:34 +0530

    Time: 0 ms (100.00%) | Memory: 13.6 MB (28.83%) - LeetSync

--- 1421-find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.cpp
+++ 1421-find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.cpp
@@ -4,8 +4,20 @@ public:
         int count=0;
 
         for(auto& it : nums){
-            int numLen = to_string(it).length();
-            if((numLen & 1) == 0) count++;
+
+            //1st method---simpe to_string() function 
+            // int numLen = to_string(it).length();
+            // if((numLen & 1) == 0) count++;
+
+            //2nd method---use logical reasoning
+            // if((it>9 && it<100) || (it>999 && it<10000) || it==100000){
+            //     count++;
+            // } 
+
+            //3rd method---use logbase10 to get ActualDigits-1 then add +1
+            int digits = (int)log10(it)+1;
+            if((digits&1)==0) count++;
+
         }
 
         return count;
