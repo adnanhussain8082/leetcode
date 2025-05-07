bed8a1448d9157673f8fc0f4973410ba19002b73
Adnan <120333722+adnanhussain8082@users.noreply.github.com>
2025-05-07 22:55:48 +0530

    Time: 65 ms (9.38%) | Memory: 37.3 MB (8.77%) - LeetSync

---
+++ 3627-find-minimum-time-to-reach-last-room-i/find-minimum-time-to-reach-last-room-i.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int minTimeToReach(vector<vector<int>>& moveTime) {
+        int n=moveTime.size() , m=moveTime[0].size();
+
+        vector<vector<int>> time(n , vector<int>(m , INT_MAX));
+        priority_queue<vector<int> , vector<vector<int>> , greater<vector<int>>> minHeap;
+
+        minHeap.push({0 , 0 , 0});
+        moveTime[0][0]=0;
+
+        vector<vector<int>> dir = {{0,1},{-1,0},{0,-1},{1,0}};
+        while(!minHeap.empty()){
+            auto it = minHeap.top();
+            minHeap.pop();
+            int currTime = it[0];
+            int i = it[1] , j = it[2];
+
+            if(currTime >= time[i][j]) continue;
+
+            time[i][j] = currTime;
+
+            if(i==n-1 && j==m-1) return currTime;
+
+
+            for(auto& it : dir){
+                int nrow = i + it[0];
+                int ncol = j + it[1];
+
+                if(nrow >=0 && nrow<n && ncol>=0 && ncol<m && time[nrow][ncol]==INT_MAX){
+                    int nextTime = max(currTime , moveTime[nrow][ncol]) + 1;
+                    minHeap.push({nextTime , nrow , ncol});
+                }
+            }
+        }
+        return time[n-1][m-1];
+    }
+};
