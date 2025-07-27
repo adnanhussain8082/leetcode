7f7e3f78df94a3a487eb3d3d06a0294663503f6d
Adnan <120333722+adnanhussain8082@users.noreply.github.com>
2025-07-27 11:58:43 +0530

    Time: 0 ms (100.00%) | Memory: 18.6 MB (79.04%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,3 +1,51 @@
+// class Solution {
+// public:
+
+//     // /Basic explanation:
+//     // /They are using the first row and column as a memory to keep track of all the 
+//     // /0's in the entire matrix.
+
+//     void setZeroes(vector<vector<int>>& matrix) {
+//         bool fr=false , fc=false;
+//         int m=matrix.size() , n=matrix[0].size();
+
+//         for(int i=0;i<m;i++){
+//             for(int j=0;j<n;j++){
+//                 if(matrix[i][j]==0){
+//                     if(i==0) fr=true;
+//                     if(j==0) fc=true;
+//                     matrix[0][j]=0; //mark in the first row
+//                     matrix[i][0]=0; //mark in the first col
+//                 }
+//             }
+//         }
+
+//         //now start from ist row and ist col, and using them start putting zeros
+//         for(int i=1;i<m;i++){
+//             for(int j=1;j<n;j++){
+//                 if(matrix[0][j]==0 || matrix[i][0]==0){
+//                     matrix[i][j]=0;
+//                 }
+//             }
+//         }
+
+//         //now deal the with first row and first col
+//         if(fr){
+//             for(int j=0;j<n;j++){
+//                 matrix[0][j]=0;
+//             }
+//         }
+
+//         if(fc){
+//             for(int i=0;i<m;i++){
+//                 matrix[i][0]=0;
+//             }
+//         }
+//     }
+// };
+
+
+
 class Solution {
 public:
 
@@ -14,22 +62,22 @@ public:
                 if(matrix[i][j]==0){
                     if(i==0) fr=true;
                     if(j==0) fc=true;
-                    matrix[0][j]=0; //mark in the first row
-                    matrix[i][0]=0; //mark in the first col
+
+                    matrix[0][j]=0;
+                    matrix[i][0]=0;
+
                 }
             }
         }
 
-        //now start from ist row and ist col, and using them start putting zeros
         for(int i=1;i<m;i++){
             for(int j=1;j<n;j++){
-                if(matrix[0][j]==0 || matrix[i][0]==0){
+                if(matrix[0][j]==0 or matrix[i][0]==0){
                     matrix[i][j]=0;
                 }
             }
         }
 
-        //now deal the with first row and first col
         if(fr){
             for(int j=0;j<n;j++){
                 matrix[0][j]=0;
