389088654802ca6ddb9ae5509f30c6b56c7083b1
Adnan <120333722+adnanhussain8082@users.noreply.github.com>
2025-09-05 14:43:16 +0530

    Time: 4 ms (83.22%) | Memory: 34.9 MB (91.51%) - LeetSync

--- 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.cpp
+++ 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.cpp
@@ -1,39 +1,42 @@
 class Solution {
 public:
     int countPaths(int n, vector<vector<int>>& roads) {
-        vector<pair<int,long long>> adj[n];
-        for(auto it : roads){
+        vector<vector<pair<int,int>>> adj(n);
+
+        for(auto& it : roads){
             adj[it[0]].push_back({it[1],it[2]});
             adj[it[1]].push_back({it[0],it[2]});
         }
-        priority_queue<pair<long long,int>,vector<pair<long long,int>>,greater<pair<long long,int>>> minHeap;
-        vector<long long> time(n,1e18);
-        vector<long long> ways(n,0);
+
+        priority_queue<pair<long long,int> , vector<pair<long long,int>> , greater<pair<long long,int>>> minHeap;
         minHeap.push({0,0});
+
+        vector<long long> time(n , 1e18);
         time[0]=0;
+
+        vector<long long> ways(n , 0);
         ways[0]=1;
 
-        int mod=(int)(1e9+7);
+        int mod = (int)(1e9+7);
 
         while(!minHeap.empty()){
-            long long timee=minHeap.top().first;
-            int node=minHeap.top().second;
+            long long t = minHeap.top().first;
+            int node = minHeap.top().second;
             minHeap.pop();
 
-            for(auto it : adj[node]){
-                int adjNode=it.first;
-                long long edgeWt=it.second;
-
-                if(timee+edgeWt < time[adjNode]){
-                    time[adjNode]=timee+edgeWt;
-                    minHeap.push({time[adjNode],adjNode});
-                    ways[adjNode]=ways[node];
+            for(auto [ne , wt] : adj[node]){
+                if(t+wt < time[ne]){
+                    time[ne] = t+wt;
+                    ways[ne] = ways[node];
+                    minHeap.push({t+wt , ne});
                 }
-                else if(timee+edgeWt == time[adjNode]){
-                    ways[adjNode]=(ways[adjNode]+ways[node])%mod;
+
+                else if(t+wt == time[ne]){
+                    ways[ne] = (ways[ne] + ways[node])%mod;
                 }
             }
         }
+
         return ways[n-1]%mod;
     }
 };
