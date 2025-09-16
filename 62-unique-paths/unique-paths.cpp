35ddde38e417b7266d186d07eb62f929856936ee
Adnan <120333722+adnanhussain8082@users.noreply.github.com>
2025-09-16 22:33:52 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (33.88%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,49 +1,77 @@
 // class Solution {  //recursion
 // private:
-//     int f(int row , int col , int rows , int cols){
+//     int f(int row , int col){
 //         //base case
-//         if(row==rows and col==cols) return 1;
+//         if(row==0 and col==0) return 1;
+
+//         if(row < 0 or col < 0) return 0;
 
 //         int cnt=0;
-//         if(row+1 <= rows){
-//             cnt += f(row+1 , col , rows , cols);
-//         }
-//         if(col+1 <= cols){
-//             cnt += f(row , col+1 , rows , cols);
-//         }
+//         int up = f(row-1 , col);
+//         int down = f(row , col-1);
+
+//         cnt += up+down;
 
 //         return cnt;
 //     }
 // public:
 //     int uniquePaths(int m, int n) {
-//         return f(0 , 0 , m-1 , n-1);
+//         return f(m-1 , n-1);
 //     }
 // };
 
 
-
 class Solution {  //memoization
 private:
-    int f(int row , int col , int rows , int cols , vector<vector<int>>& dp){
+    int f(int row , int col , vector<vector<int>>& dp){
         //base case
-        if(row==rows and col==cols) return 1;
+        if(row==0 and col==0) return 1;
 
-        if(dp[row][col]!=-1) return dp[row][col];
+        if(row < 0 or col < 0) return 0;
+
+        if(dp[row][col] != -1) return dp[row][col];
+
         int cnt=0;
-        
-        if(row+1 <= rows){
-            cnt += f(row+1 , col , rows , cols , dp);
-        }
-        if(col+1 <= cols){
-            cnt += f(row , col+1 , rows , cols , dp);
-        }
+        int up = f(row-1 , col , dp);
+        int down = f(row , col-1 , dp);
+
+        cnt += up+down;
 
         return dp[row][col] = cnt;
     }
 public:
     int uniquePaths(int m, int n) {
         vector<vector<int>> dp(m , vector<int>(n,-1));
-
-        return f(0 , 0 , m-1 , n-1 , dp);
+        return f(m-1 , n-1 , dp);
     }
 };
+
+
+
+// class Solution {  //memoization
+// private:
+//     int f(int row , int col , int rows , int cols , vector<vector<int>>& dp){
+//         //base case
+//         if(row==rows and col==cols) return 1;
+
+//         if(dp[row][col]!=-1) return dp[row][col];
+//         int cnt=0;
+        
+//         if(row+1 <= rows){
+//             cnt += f(row+1 , col , rows , cols , dp);
+//         }
+//         if(col+1 <= cols){
+//             cnt += f(row , col+1 , rows , cols , dp);
+//         }
+
+//         return dp[row][col] = cnt;
+//     }
+// public:
+//     int uniquePaths(int m, int n) {
+//         vector<vector<int>> dp(m , vector<int>(n,-1));
+
+//         return f(0 , 0 , m-1 , n-1 , dp);
+//     }
+// };
+
+
