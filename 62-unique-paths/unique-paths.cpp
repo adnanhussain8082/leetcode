5df71548fdf99a73efd66bde0be86c311003b328
Adnan <120333722+adnanhussain8082@users.noreply.github.com>
2025-09-17 00:23:04 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (81.18%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -21,34 +21,34 @@
 // };
 
 
-class Solution {  //memoization
-private:
-    int f(int row , int col , vector<vector<int>>& dp){
-        //base case
-        if(row==0 and col==0) return 1;
+// class Solution {  //memoization--one way
+// private:
+//     int f(int row , int col , vector<vector<int>>& dp){
+//         //base case
+//         if(row==0 and col==0) return 1;
 
-        if(row < 0 or col < 0) return 0;
+//         if(row < 0 or col < 0) return 0;
 
-        if(dp[row][col] != -1) return dp[row][col];
+//         if(dp[row][col] != -1) return dp[row][col];
 
-        int cnt=0;
-        int up = f(row-1 , col , dp);
-        int down = f(row , col-1 , dp);
+//         int cnt=0;
+//         int up = f(row-1 , col , dp);
+//         int down = f(row , col-1 , dp);
 
-        cnt += up+down;
+//         cnt += up+down;
 
-        return dp[row][col] = cnt;
-    }
-public:
-    int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m , vector<int>(n,-1));
-        return f(m-1 , n-1 , dp);
-    }
-};
+//         return dp[row][col] = cnt;
+//     }
+// public:
+//     int uniquePaths(int m, int n) {
+//         vector<vector<int>> dp(m , vector<int>(n,-1));
+//         return f(m-1 , n-1 , dp);
+//     }
+// };
 
 
 
-// class Solution {  //memoization
+// class Solution {  //memoization--other way
 // private:
 //     int f(int row , int col , int rows , int cols , vector<vector<int>>& dp){
 //         //base case
@@ -75,3 +75,66 @@ public:
 // };
 
 
+// class Solution {  //tabulation
+// public:
+//     int uniquePaths(int m, int n) {
+//         vector<vector<int>> dp(m , vector<int>(n,0));
+
+//         for(int row=0;row<m;row++){
+//             for(int col=0;col<n;col++){
+//                 if(row==0 and col==0){
+//                     dp[row][col]=1;
+//                     continue;
+//                 }
+
+//                 int up=0;
+//                 int left=0;
+
+//                 if(row > 0){
+//                     up = dp[row-1][col];
+//                 }
+
+//                 if(col > 0){
+//                     left = dp[row][col-1];
+//                 }
+
+//                 dp[row][col]=up+left;
+//             }
+//         }
+//         return dp[m-1][n-1];
+//     }
+// };
+
+
+class Solution {  //space optimization
+public:
+    int uniquePaths(int m, int n) {
+        vector<int> prev(n,0);
+
+        for(int row=0;row<m;row++){
+            vector<int> temp(n,0);
+            for(int col=0;col<n;col++){
+                if(row==0 and col==0){
+                    temp[col]=1;
+                    continue;
+                }
+
+                int up=0;
+                int left=0;
+
+                if(row > 0){
+                    up = prev[col];
+                }
+
+                if(col > 0){
+                    left = temp[col-1];
+                }
+
+                temp[col]=up+left;
+            }
+
+            prev = temp;
+        }
+        return prev[n-1];
+    }
+};
