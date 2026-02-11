83949d21eb4e2a26286fe72016a6de5a19f642bc
Adnan <120333722+adnanhussain8082@users.noreply.github.com>
2026-02-11 19:59:05 +0530

    Time: 2 ms (20.02%) | Memory: 9.4 MB (28.96%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -10,15 +10,31 @@ private:
     // }
 
     //memo
-    int f(int m , int n , vector<vector<int>>& dp){
-        //base cases
-        if(m==0 and n==0) return 1;
-        if(m<0 or n<0) return 0;
+    // int f(int m , int n , vector<vector<int>>& dp){
+    //     //base cases
+    //     if(m==0 and n==0) return 1;
+    //     if(m<0 or n<0) return 0;
 
-        if(dp[m][n]!=-1) return dp[m][n];
+    //     if(dp[m][n]!=-1) return dp[m][n];
 
-        return dp[m][n] = f(m-1 , n , dp) + f(m , n-1 , dp);
+    //     return dp[m][n] = f(m-1 , n , dp) + f(m , n-1 , dp);
+    // }
+
+    //tab
+    int f(int& m , int& n){
+        vector<vector<int>> dp(m , vector<int>(n,1));
+
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            }
+        }
+
+        return dp[m-1][n-1];
     }
+
+
+
 
 public:
     int uniquePaths(int m, int n) {
@@ -26,8 +42,11 @@ public:
         // return f(m-1,n-1);
 
         //memo
-        vector<vector<int>> dp(m , vector<int>(n,-1));
-        return f(m-1,n-1,dp);
+        // vector<vector<int>> dp(m , vector<int>(n,-1));
+        // return f(m-1,n-1,dp);
+
+        //tab
+        return f(m , n);
 
     }
 };
