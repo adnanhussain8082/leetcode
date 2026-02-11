36725c13e6dfc709381266e68679e566591fff48
Adnan <120333722+adnanhussain8082@users.noreply.github.com>
2026-02-11 20:04:03 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (76.38%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -21,17 +21,33 @@ private:
     // }
 
     //tab
+    // int f(int& m , int& n){
+    //     vector<vector<int>> dp(m , vector<int>(n,1));
+
+    //     for(int i=1;i<m;i++){
+    //         for(int j=1;j<n;j++){
+    //             dp[i][j] = dp[i-1][j] + dp[i][j-1];
+    //         }
+    //     }
+
+    //     return dp[m-1][n-1];
+    // }
+
+    //SO
     int f(int& m , int& n){
-        vector<vector<int>> dp(m , vector<int>(n,1));
+        vector<int> prev(n,1);
 
         for(int i=1;i<m;i++){
+            vector<int> curr(n,1);
             for(int j=1;j<n;j++){
-                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+                curr[j] = prev[j] + curr[j-1];
             }
+            prev=curr;
         }
 
-        return dp[m-1][n-1];
+        return prev[n-1];
     }
+
 
 
 
@@ -46,7 +62,10 @@ public:
         // return f(m-1,n-1,dp);
 
         //tab
-        return f(m , n);
+        // return f(m , n);
+
+        //SO
+        return f(m,n);
 
     }
 };
