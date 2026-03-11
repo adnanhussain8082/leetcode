e76e267a96bc39f3262ebf3d2aa8d9898bce00b5
Adnan <120333722+adnanhussain8082@users.noreply.github.com>
2026-03-11 20:41:37 +0530

    Time: 13 ms (57.47%) | Memory: 19.1 MB (65.31%) - LeetSync

--- 116-populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.cpp
+++ 116-populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.cpp
@@ -16,32 +16,60 @@ public:
 };
 */
 
+//iterative approach o(n) space
+// class Solution {
+// public:
+//     Node* connect(Node* root) {
+//         // Node* cur = root;
+//         if(root==NULL) return NULL;
+//         queue<Node*> q;
+//         q.push(root);
+
+//         while(!q.empty()){
+//             int sz = q.size();
+
+//             for(int i=0;i<sz;i++){
+//                 Node* cur = q.front();
+//                 q.pop();
+
+//                 if(cur->left) q.push(cur->left);
+//                 if(cur->right) q.push(cur->right);
+
+//                 if(i==sz-1){
+//                     cur->next = NULL;
+//                 }
+//                 else{
+//                     cur->next = q.front();
+//                 }
+//             }
+//         }
+//         return root;
+//     }
+// };
+
+
+
+
+//recursive approach o(1) space 
+
 class Solution {
+private:
+    void f(Node* root){
+        if(root==NULL) return;
+
+        Node *L=root->left , *R=root->right , *N=root->next;
+
+        if(L){
+            L->next = R;
+            if(N) R->next = N->left;
+
+            f(root->left);
+            f(root->right);
+        }
+    }
 public:
     Node* connect(Node* root) {
-        // Node* cur = root;
-        if(root==NULL) return NULL;
-        queue<Node*> q;
-        q.push(root);
-
-        while(!q.empty()){
-            int sz = q.size();
-
-            for(int i=0;i<sz;i++){
-                Node* cur = q.front();
-                q.pop();
-
-                if(cur->left) q.push(cur->left);
-                if(cur->right) q.push(cur->right);
-
-                if(i==sz-1){
-                    cur->next = NULL;
-                }
-                else{
-                    cur->next = q.front();
-                }
-            }
-        }
+        f(root);
         return root;
     }
 };
