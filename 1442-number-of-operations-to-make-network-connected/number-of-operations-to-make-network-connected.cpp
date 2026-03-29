110ddf50c95b5523e8033ceb2c116f7f71e8bc6b
Adnan <120333722+adnanhussain8082@users.noreply.github.com>
2026-03-29 19:57:49 +0530

    Time: 19 ms (53.06%) | Memory: 46.6 MB (51.34%) - LeetSync

--- 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.cpp
+++ 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.cpp
@@ -1,43 +1,32 @@
-class DisjointSet{
-    public:
-
-    vector<int> size , parent;
-    DisjointSet(int n){
-        size.resize(n+1,1);
+class DisjointSet {
+public: 
+    vector<int> parent, size; 
+    DisjointSet(int n) {
         parent.resize(n+1);
-        for(int i=0;i<=n;i++){
-            parent[i]=i;
+        size.resize(n+1 , 1); 
+        for(int i = 0; i <= n; i++) {
+            parent[i] = i; 
+            // size[i] = 1; 
         }
     }
 
-    int findUPar(int node){
-        if(node==parent[node]){
-            return node;
-        }
-
-        return parent[node] = findUPar(parent[node]);
+    int findUPar(int node) {
+        if(node == parent[node])
+            return node; 
+        return parent[node] = findUPar(parent[node]); 
     }
 
-    int minEdges=0;
-    void UnionBySize(int& u , int& v){
-        int ulp_u = findUPar(u);
+    void unionBySize(int u, int v) {
+        int ulp_u = findUPar(u); 
         int ulp_v = findUPar(v);
-
-        if(ulp_u == ulp_v){
-           return;
+        if(ulp_u == ulp_v) return; 
+        if(size[ulp_u] < size[ulp_v]) {
+            parent[ulp_u] = ulp_v; 
+            size[ulp_v] += size[ulp_u]; 
         }
-
-        if(size[ulp_u] < size[ulp_v]){
-            parent[ulp_u] = ulp_v;
-            size[ulp_v] += size[ulp_u];
-        }
-        else if(size[ulp_u] > size[ulp_v]){
+        else {
             parent[ulp_v] = ulp_u;
-            size[ulp_u] += size[ulp_v];
-        }
-        else{
-            parent[ulp_v] = ulp_u;
-            size[ulp_u]++;
+            size[ulp_u] += size[ulp_v]; 
         }
     }
 };
@@ -46,27 +35,27 @@ public:
     int makeConnected(int n, vector<vector<int>>& connections) {
         DisjointSet ds(n);
         int ExtraEdges=0;
+        for(auto it : connections){
+            int u=it[0];
+            int v=it[1];
 
-        for(auto& it : connections){
-            int u = it[0];
-            int v = it[1];
-
-            if(ds.findUPar(u) == ds.findUPar(v)){
+            if(ds.findUPar(u)==ds.findUPar(v)){
                 ExtraEdges++;
             }
             else{
-                ds.UnionBySize(u,v);
+                ds.unionBySize(u,v);
             }
         }
 
-        int nc=0;
+        int NumComp=0; //NumComp = no of components
+        //now traverse thru parent vector to find the no of ultimate parents which inturn gives us no of components
         for(int i=0;i<n;i++){
-            if(ds.parent[i] == i){
-                nc++;
+            if(i == ds.parent[i]){
+                NumComp++;
             }
         }
 
-        int ans=nc-1;
+        int ans=NumComp-1;
 
         if(ExtraEdges >= ans){
             return ans;
