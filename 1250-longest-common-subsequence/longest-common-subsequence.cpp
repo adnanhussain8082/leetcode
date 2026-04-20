67011a68846a32663a06859594b6a966461ecf88
Adnan <120333722+adnanhussain8082@users.noreply.github.com>
2026-04-20 22:58:42 +0530

    Time: 43 ms (22.67%) | Memory: 27.8 MB (5.82%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,25 +1,18 @@
-// class Solution { recursion
+// class Solution { 
 // private:
-//     int f(int i , int j , string& t1 , string& t2){
+//     int f(int i , int j , string& text1 , string& text2){
 //         //base case
 //         if(i<0 or j<0) return 0;
 
-//         int ans=0;
-
-//         if(t1[i]==t2[j]){
-//             ans = 1 + f(i-1 , j-1 , t1 , t2);
-//         }
-//         else{
-//             ans = max(f(i-1,j,t1,t2) , f(i,j-1,t1,t2));
+//         if(text1[i]==text2[j]){
+//             return 1 + f(i-1 , j-1 , text1 , text2);
 //         }
 
-//         return ans;
+//         return max(f(i-1 , j , text1 , text2) , f(i , j-1 , text1 , text2));
 //     }
 // public:
 //     int longestCommonSubsequence(string text1, string text2) {
-//         int m=text1.size() , n=text2.size();
-
-//         return f(m-1,n-1,text1,text2);
+//         return f(text1.size()-1 , text2.size()-1 , text1 , text2);
 //     }
 // };
 
@@ -27,79 +20,23 @@
 
 
 
-// class Solution {   //recursion with shifted index
+
+
+// class Solution { //shifted index
 // private:
-//     int f(int i , int j , string& t1 , string& t2){
+//     int f(int i , int j , string& text1 , string& text2){
 //         //base case
 //         if(i==0 or j==0) return 0;
 
-
-//         if(t1[i-1]==t2[j-1]){
-//             return 1 + f(i-1 , j-1 , t1 , t2);
+//         if(text1[i-1]==text2[j-1]){
+//             return 1 + f(i-1 , j-1 , text1 , text2);
 //         }
-        
-//         return max(f(i-1,j,t1,t2) , f(i,j-1,t1,t2));
 
+//         return max(f(i-1 , j , text1 , text2) , f(i , j-1 , text1 , text2));
 //     }
 // public:
 //     int longestCommonSubsequence(string text1, string text2) {
-//         int m=text1.size() , n=text2.size();
-
-//         return f(m,n,text1,text2);
-//     }
-// };
-
-
-
-// class Solution {   //memoization with shifted index(shifted index is required in case of tabulation cuz 2d matrix can't have negative index as is seen in the base case of simple recursion)
-// private:
-//     int f(int i , int j , string& t1 , string& t2 , vector<vector<int>>& dp){
-//         //base case
-//         if(i==0 or j==0) return 0;
-
-//         if(dp[i][j]!=-1) return dp[i][j];
-
-//         if(t1[i-1]==t2[j-1]){
-//             return dp[i][j] = 1 + f(i-1 , j-1 , t1 , t2 , dp);
-//         }
-        
-//         return dp[i][j] = max(f(i-1,j,t1,t2,dp) , f(i,j-1,t1,t2,dp));
-
-//     }
-// public:
-//     int longestCommonSubsequence(string text1, string text2) {
-//         int m=text1.size() , n=text2.size();
-//         vector<vector<int>> dp(m+1,vector<int>(n+1,-1));
-
-//         return f(m,n,text1,text2,dp);
-//     }
-// };
-
-
-
-
-// class Solution {   //tabulation with shifted index(shifted index is required in case of tabulation cuz 2d matrix can't have negative index as is seen in the base case of simple recursion)
-
-// public:
-//     int longestCommonSubsequence(string text1, string text2) {
-//         int m=text1.size() , n=text2.size();
-//         vector<vector<int>> dp(m+1,vector<int>(n+1,0));
-
-//         for(int i=1;i<m+1;i++){
-//             for(int j=1;j<n+1;j++){
-//                 int ans=0;
-//                 if(text1[i-1]==text2[j-1]){  //shifted index
-//                     ans += 1 + dp[i-1][j-1];
-//                 }
-//                 else{
-//                     ans += max(dp[i-1][j] , dp[i][j-1]);
-//                 }
-
-//                 dp[i][j] = ans;
-//             }
-//         }
-
-//         return dp[m][n];
+//         return f(text1.size() , text2.size() , text1 , text2);
 //     }
 // };
 
@@ -107,36 +44,26 @@
 
 
 
-class Solution {   //space optmization
 
+
+class Solution { //shifted index and memorization
+private:
+    int f(int i , int j , string& text1 , string& text2 , vector<vector<int>>& dp){
+        //base case
+        if(i==0 or j==0) return 0;
+
+        if(dp[i][j]!=-1) return dp[i][j];
+
+        if(text1[i-1]==text2[j-1]){
+            return dp[i][j] = 1 + f(i-1 , j-1 , text1 , text2 , dp);
+        }
+
+        return dp[i][j] = max(f(i-1 , j , text1 , text2 , dp) , f(i , j-1 , text1 , text2 , dp));
+    }
 public:
     int longestCommonSubsequence(string text1, string text2) {
         int m=text1.size() , n=text2.size();
-        
-        vector<int> prev(n+1,0);
-
-        // prev[0]=0;
-
-        // for(int j=0;j<n+1;j++){
-        //     prev[j] = 0;
-        // }
-
-        for(int i=1;i<m+1;i++){
-            vector<int> temp(n+1,0);
-            for(int j=1;j<n+1;j++){
-                int ans=0;
-                if(text1[i-1]==text2[j-1]){  //shifted index
-                    ans += 1 + prev[j-1];
-                }
-                else{
-                    ans += max(prev[j] , temp[j-1]);
-                }
-
-                temp[j] = ans;
-            }
-            prev=temp;
-        }
-
-        return prev[n];
+        vector<vector<int>> dp(m+1 , vector<int>(n+1 , -1));
+        return f(m , n , text1 , text2 , dp);
     }
 };
