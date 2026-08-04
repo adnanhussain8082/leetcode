d45bc0aeeb21c6445256459afb93ea8d37305510
Adnan <120333722+adnanhussain8082@users.noreply.github.com>
2026-08-04 22:22:16 +0530

    Time: 92 ms (29.02%) | Memory: 47.8 MB (30.65%) - LeetSync

--- 744-network-delay-time/network-delay-time.cpp
+++ 744-network-delay-time/network-delay-time.cpp
@@ -1,32 +1,37 @@
 class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        vector<vector<pair<int,int>>> adj(n+1);
-        for(auto& it : times){
+        vector<pair<int,int>> adj[n+1];
+        for(auto it : times){
             adj[it[0]].push_back({it[1],it[2]});
         }
-
-        vector<int> time(n+1 , 1e9);
-        time[k]=0;
-
-        priority_queue<pair<int,int> , vector<pair<int,int>> , greater<pair<int,int>>> minHeap;
-        minHeap.push({0,k});
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> minHeap;
+        vector<int> tym(n+1,1e9); //tym=time
+        minHeap.push({0,k});  //k=src
+        tym[k]=0;
 
         while(!minHeap.empty()){
-            int t = minHeap.top().first;
-            int node =minHeap.top().second;
+            auto it=minHeap.top();
             minHeap.pop();
 
-            for(auto [ne , wt] : adj[node]){
-                if(t+wt < time[ne]){
-                    time[ne] = t+wt;
-                    minHeap.push({time[ne] , ne});
+            int timee=it.first;
+            int node=it.second;
+
+            for(auto it : adj[node]){
+                int adjNode=it.first;
+                int edgeWt=it.second;
+
+                if(timee+edgeWt < tym[adjNode]){
+                    tym[adjNode]=timee+edgeWt;
+                    minHeap.push({tym[adjNode],adjNode});
                 }
             }
         }
 
-        int maxTime = *max_element(time.begin()+1 , time.end());
+        auto maxTime = *(max_element(tym.begin()+1,tym.end())); 
+        //+1 bcoz there is 0 index also present and it 
+        //will always have value=1e9 bcoz the nodes are labelled from 1 to n
 
-        return maxTime==1e9 ? -1 : maxTime;
+        return (maxTime==1e9) ? -1 : maxTime;
     }
 };
